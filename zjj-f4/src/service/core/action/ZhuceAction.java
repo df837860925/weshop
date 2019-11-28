88e262df889f7fc5773fa3f018d571adb01840d8
fengerleng <1531622132@qq.com>
2019-11-28 16:49:15 +0800

    之前的提交没有成功，这是注册添加了验证码修改了部分样式以及mvc部分逻辑。

---
+++ zjj-f4/src/service/core/action/ZhuceAction.java
@@ -0,0 +1,30 @@
+package service.core.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.core.Action;
+import service.core.ActionForm;
+import service.core.ActionForword;
+import service.core.form.ZhuceActionForm;
+
+public class ZhuceAction extends Action{
+	@Override
+	public ActionForword execute(HttpServletRequest request,
+			HttpServletResponse response, ActionForm actionForm)
+			throws ServletException, IOException {
+		String yanzhengma = (String)request.getSession().getAttribute("code");
+		ZhuceActionForm af = (ZhuceActionForm)actionForm;
+		if (af.getYanZhengma().toUpperCase().equals(request.getSession().getAttribute("code").toString())) {
+			System.out.println("验证码正确");
+		}
+		else{
+			System.out.println("验证码错误");
+		}
+		System.out.println(af);
+		return null;
+	}
+}
